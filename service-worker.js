35bc8fa4e791b4c30ee4b2cddaab8c35b7b89e88
Hernando Vega <hernando.vega@uniminuto.edu.co>
2025-04-27 22:43:12 -0500

    Versionar cache y limpiar SW antiguo

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,42 @@
-self.addEventListener('install', (e) => {
-    console.log('Service Worker instalado');
-    e.waitUntil(
-        caches.open('renttech-cache').then((cache) => {
-            return cache.addAll([
-                './',
-                './index.html',
-                './css/style.css',
-                './dist/script.js',
-                './manifest.json'
-            ]);
-        })
-    );
+// service-worker.js
+
+const CACHE_NAME = 'renttech-cache-v2';    // ← incrementa este sufijo cuando cambies
+const ASSETS = [
+  './',
+  './index.html',
+  './css/style.css',
+  './dist/script.js',
+  './manifest.json',
+  './images/icon-192.png',
+  './images/icon-512.png'
+];
+
+// Al instalar, guarda todos los assets
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((response) => {
-            return response || fetch(e.request);
-        })
-    );
+// Al activar, borra caches viejos
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+// Al hacer fetch, responde desde caché o va a la red
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => res || fetch(e.request))
+  );
 });
